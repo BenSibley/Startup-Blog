c440b4a46fc218be669c10e299041aa154cd8f01
BenSibley <ben@competethemes.com>
2017-06-04 14:28:20 -0400

    fonts and font weights

--- inc/scripts.php
+++ inc/scripts.php
@@ -3,10 +3,10 @@
 // Front-end scripts
 function ct_business_blog_load_scripts_styles() {
 
-	wp_enqueue_style( 'ct-business_blog-google-fonts', '//fonts.googleapis.com/css?family=Playfair+Display:400|Raleway:400,700,400italic' );
+	wp_enqueue_style( 'ct-business-blog-google-fonts', '//fonts.googleapis.com/css?family=Montserrat:400|Source+Sans+Pro:400,400italic,700' );
 
-	wp_enqueue_script( 'ct-business_blog-js', get_template_directory_uri() . '/js/build/production.min.js', array( 'jquery' ), '', true );
-	wp_localize_script( 'ct-business_blog-js', 'objectL10n', array(
+	wp_enqueue_script( 'ct-business-blog-js', get_template_directory_uri() . '/js/build/production.min.js', array( 'jquery' ), '', true );
+	wp_localize_script( 'ct-business-blog-js', 'objectL10n', array(
 		'openMenu'       => __( 'open menu', 'business-blog' ),
 		'closeMenu'      => __( 'close menu', 'business-blog' ),
 		'openChildMenu'  => __( 'open dropdown menu', 'business-blog' ),
@@ -15,28 +15,28 @@ function ct_business_blog_load_scripts_styles() {
 
 	wp_enqueue_style( 'font-awesome', get_template_directory_uri() . '/assets/font-awesome/css/font-awesome.min.css' );
 
-	wp_enqueue_style( 'ct-business_blog-style', get_stylesheet_uri() );
+	wp_enqueue_style( 'ct-business-blog-style', get_stylesheet_uri() );
 
 	// enqueue comment-reply script only on posts & pages with comments open ( included in WP core )
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
 	}
 }
-add_action( 'wp_enqueue_scripts', 'ct_business_blog_load_scripts_styles' );
+add_action( 'wp_enqueue_scripts', 'ct_business-blog_load_scripts_styles' );
 
 // Back-end scripts
 function ct_business_blog_enqueue_admin_styles( $hook ) {
 
-	if ( $hook == 'appearance_page_business_blog-options' ) {
-		wp_enqueue_style( 'ct-business_blog-admin-styles', get_template_directory_uri() . '/styles/admin.min.css' );
+	if ( $hook == 'appearance_page_business-blog-options' ) {
+		wp_enqueue_style( 'ct-business-blog-admin-styles', get_template_directory_uri() . '/styles/admin.min.css' );
 	}
 }
 add_action( 'admin_enqueue_scripts', 'ct_business_blog_enqueue_admin_styles' );
 
 // Customizer scripts
 function ct_business_blog_enqueue_customizer_scripts() {
-	wp_enqueue_script( 'ct-business_blog-customizer-js', get_template_directory_uri() . '/js/build/customizer.min.js', array( 'jquery' ), '', true );
-	wp_enqueue_style( 'ct-business_blog-customizer-styles', get_template_directory_uri() . '/styles/customizer.min.css' );
+	wp_enqueue_script( 'ct-business-blog-customizer-js', get_template_directory_uri() . '/js/build/customizer.min.js', array( 'jquery' ), '', true );
+	wp_enqueue_style( 'ct-business-blog-customizer-styles', get_template_directory_uri() . '/styles/customizer.min.css' );
 }
 add_action( 'customize_controls_enqueue_scripts', 'ct_business_blog_enqueue_customizer_scripts' );
 
@@ -45,7 +45,7 @@ add_action( 'customize_controls_enqueue_scripts', 'ct_business_blog_enqueue_cust
  * transport => postMessage
  */
 function ct_business_blog_enqueue_customizer_post_message_scripts() {
-	wp_enqueue_script( 'ct-business_blog-customizer-post-message-js', get_template_directory_uri() . '/js/build/postMessage.min.js', array( 'jquery' ), '', true );
+	wp_enqueue_script( 'ct-business-blog-customizer-post-message-js', get_template_directory_uri() . '/js/build/postMessage.min.js', array( 'jquery' ), '', true );
 
 }
 add_action( 'customize_preview_init', 'ct_business_blog_enqueue_customizer_post_message_scripts' );

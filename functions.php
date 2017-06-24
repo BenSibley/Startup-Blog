c91ac487b2d008419a874e783c91e808c1bf89b5
BenSibley <ben@competethemes.com>
2017-06-24 14:55:04 -0400

    updated custom colors to work with pro templates

--- functions.php
+++ functions.php
@@ -587,7 +587,7 @@ if ( ! function_exists( 'ct_startup_blog_override_colors' ) ) {
 			$color_css .= "@media all and (min-width: 50em) { .menu-primary-items li.menu-item-has-children:hover > a,.menu-primary-items li.menu-item-has-children:hover > a:after,.menu-primary-items a:hover:after,.menu-primary-items a:active:after,.menu-primary-items a:focus:after,.menu-secondary-items li.menu-item-has-children:hover > a,.menu-secondary-items li.menu-item-has-children:hover > a:after,.menu-secondary-items a:hover:after,.menu-secondary-items a:active:after,.menu-secondary-items a:focus:after {
 		  color: $primary_color;
 		} }";
-			$color_css .= "input[type=\"submit\"],.comment-pagination a:hover,.comment-pagination a:active,.comment-pagination a:focus,.site-header:before,.social-media-icons a:hover,.social-media-icons a:active,.social-media-icons a:focus,.pagination a:hover,.pagination a:active,.pagination a:focus,.featured-image > a:after,.entry:before,.post-tags a,.widget_calendar #prev a:hover,.widget_calendar #prev a:active,.widget_calendar #prev a:focus,.widget_calendar #next a:hover,.widget_calendar #next a:active,.widget_calendar #next a:focus,.bb-slider .image-container:after,.sticky-status span {
+			$color_css .= "input[type=\"submit\"],.comment-pagination a:hover,.comment-pagination a:active,.comment-pagination a:focus,.site-header:before,.social-media-icons a:hover,.social-media-icons a:active,.social-media-icons a:focus,.pagination a:hover,.pagination a:active,.pagination a:focus,.featured-image > a:after,.entry:before,.post-tags a,.widget_calendar #prev a:hover,.widget_calendar #prev a:active,.widget_calendar #prev a:focus,.widget_calendar #next a:hover,.widget_calendar #next a:active,.widget_calendar #next a:focus,.bb-slider .image-container:after,.sticky-status span,.overflow-container .hero-image-header:before {
 			background: $primary_color;
 		}";
 			$color_css .= "@media all and (min-width: 50em) { .menu-primary-items ul:before,.menu-secondary-items ul:before {
@@ -620,7 +620,7 @@ if ( ! function_exists( 'ct_startup_blog_override_colors' ) ) {
 		// Update gradients if either color has changed
 		if ( $primary_color != '#20a4e6' || $secondary_color != '#17e6c3' ) {
 
-			$color_css .= ".site-header:before,.featured-image > a:after,.entry:before,.bb-slider .image-container:after {
+			$color_css .= ".site-header:before,.featured-image > a:after,.entry:before,.bb-slider .image-container:after,.overflow-container .hero-image-header:before {
 		  background-image: -webkit-linear-gradient(left, $primary_color, $secondary_color);
 		  background-image: linear-gradient(to right, $primary_color, $secondary_color);
 		}";

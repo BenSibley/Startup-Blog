1c0e0964db533abb3be14bab863cf8d9619136ab
BenSibley <ben@competethemes.com>
2017-08-09 14:36:10 -0400

    added review request

--- functions.php
+++ functions.php
@@ -7,6 +7,14 @@ require_once( trailingslashit( get_template_directory() ) . 'theme-options.php' 
 foreach ( glob( trailingslashit( get_template_directory() ) . 'inc/*.php' ) as $filename ) {
 	include $filename;
 }
+require_once( trailingslashit( get_template_directory() ) . 'dnh/handler.php' );
+new WP_Review_Me( array(
+		'days_after' => 14,
+		'type'       => 'theme',
+		'slug'       => 'startup-blog',
+		'message'    => __( 'Hey! Sorry to interrupt, but you\'ve been using Startup Blog for a little while now. If you\'re happy with this theme, could you take a minute to leave a review? <i>You won\'t see this notice again after closing it.</i>', 'startup-blog' )
+	)
+);
 
 //----------------------------------------------------------------------------------
 //	Set content width variable

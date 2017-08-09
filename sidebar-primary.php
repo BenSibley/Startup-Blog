28b9ff7680483091e7a6e4d5146af30b48d18c5f
BenSibley <ben@competethemes.com>
2017-08-09 15:06:42 -0400

    added full-width template

--- sidebar-primary.php
+++ sidebar-primary.php
@@ -5,6 +5,9 @@ if ( function_exists( 'is_woocommerce' ) ) {
         return;
     }
 }
+if ( is_page_template( 'templates/full-width.php' ) ) {
+    return;
+}
 if ( is_active_sidebar( 'primary' ) ) : ?>
     <aside class="sidebar sidebar-primary" id="sidebar-primary" role="complementary">
         <?php dynamic_sidebar( 'primary' ); ?>

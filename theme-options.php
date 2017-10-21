99e1d89e51758a656203577e18a913a96b091fbf
BenSibley <ben@competethemes.com>
2017-10-21 13:57:52 -0400

    copy update

--- theme-options.php
+++ theme-options.php
@@ -78,7 +78,7 @@ if ( ! function_exists( 'ct_startup_blog_options_content' ) ) {
 				</div>
 				<div class="content content-presspad">
 					<h3><?php esc_html_e( 'Turn Startup Blog into a Mobile App', 'startup-blog' ); ?></h3>
-					<p><?php printf( esc_html__( '%s can be converted into a mobile app and listed on the App Store with the help of PressPad News. Read our tutorial to learn more.', 'startup-blog' ), wp_get_theme( get_template() ) ); ?></p>
+					<p><?php printf( esc_html__( '%s can be converted into a mobile app and listed on the App Store and Google Play Store with the help of PressPad News. Read our tutorial to learn more.', 'startup-blog' ), wp_get_theme( get_template() ) ); ?></p>
 					<a target="_blank" class="button-primary" href="https://www.competethemes.com/help/convert-mobile-app-startup-blog/"><?php esc_html_e( 'Read Tutorial', 'startup-blog' ); ?></a>
 				</div>
 				<div class="content content-delete-settings">

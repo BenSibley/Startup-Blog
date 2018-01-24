d3f910329427732129b5a3fbcbba3f93f21ad279
BenSibley <ben@competethemes.com>
2018-01-24 15:41:09 -0500

    better way to show site title when logo is removed

--- functions.php
+++ functions.php
@@ -677,3 +677,10 @@ function ct_startup_blog_no_missing_titles( $title, $id = null ) {
 	return $title;
 }
 add_filter( 'the_title', 'ct_startup_blog_no_missing_titles', 10, 2 );
+
+//----------------------------------------------------------------------------------
+// Turn of partial refresh for logo, so title shows up when logo is removed
+//----------------------------------------------------------------------------------
+add_action( 'customize_register', function( $wp_customize ) {
+	$wp_customize->get_setting( 'custom_logo' )->transport = 'refresh';
+}, 20 );

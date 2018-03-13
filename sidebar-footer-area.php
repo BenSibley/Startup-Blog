f11f9c99cebb05cce3b88ddfd64fe24b3d4c53fd
BenSibley <ben@competethemes.com>
2018-03-13 15:19:12 -0400

    small security update

--- sidebar-footer-area.php
+++ sidebar-footer-area.php
@@ -2,7 +2,7 @@
   $widgets      = get_option( 'sidebars_widgets' );
 	$widget_class = count( $widgets['footer-area'] );
   ?>
-	<aside id="sidebar-footer-area" class="sidebar widget-area sidebar-footer-area active-<?php echo $widget_class; ?>" role="complementary">
+	<aside id="sidebar-footer-area" class="sidebar widget-area sidebar-footer-area active-<?php echo esc_attr($widget_class); ?>" role="complementary">
 		<?php dynamic_sidebar( 'footer-area' ); ?>
 	</aside>
 <?php endif;

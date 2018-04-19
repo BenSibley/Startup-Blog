9be1535181c9bf314f95cb02e7b2f970701fde1f
BenSibley <ben@competethemes.com>
2018-04-19 09:16:54 -0400

    removing button text in slider now removes the button

--- content-slide.php
+++ content-slide.php
@@ -15,16 +15,17 @@ $button_text = get_theme_mod('slider_button_text');
 			// echo'ing get_the_excerpt() instead of using the_excerpt() to avoid plugins adding content via filters.
 			// Ex. Jetpack will add social sharing buttons into the slide when using the_excerpt(): http://pics.competethemes.com/l3AM
 			echo wp_kses_post( wpautop( get_the_excerpt() ) );
-			?>
-			<a class="read-more" href="<?php the_permalink(); ?>">
-				<?php
-				if ( $button_text == '' ) {
-					esc_html_e( 'Read more', 'startup-blog');
-				} else {
-					echo esc_html( $button_text );
-				}
-				?>
-			</a>
+			if ( $button_text ) :?>
+				<a class="read-more" href="<?php the_permalink(); ?>">
+					<?php
+					if ( $button_text == '' ) {
+						esc_html_e( 'Read more', 'startup-blog');
+					} else {
+						echo esc_html( $button_text );
+					}
+					?>
+				</a>
+			<?php endif; ?>
 		</div>
 	</div>
 	<div class="image-container" style="background-image: url('<?php the_post_thumbnail_url(); ?>');"></div>

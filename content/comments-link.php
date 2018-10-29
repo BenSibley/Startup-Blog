b2f8485f9e347a055697f5e0703c3453c467484c
BenSibley <ben@competethemes.com>
2018-10-29 11:05:40 -0400

    comment icon not showing after FA update

--- content/comments-link.php
+++ content/comments-link.php
@@ -2,16 +2,16 @@
 if ( get_theme_mod( 'comment_link' ) == 'no' ) return;
 
 $classes = 'post-comments-link';
-$icon = 'fa-comment-o';
+$icon = 'far fa-comment';
 if ( !comments_open() ) {
 	$classes .= ' closed';
-	$icon = 'fa-comment';
+	$icon = 'fas fa-comment';
 }
 ?>
 <span class="<?php echo esc_attr( $classes ); ?>">
 	<?php
 	echo '<a href="' . esc_url( get_comments_link() ) . '">';
-		echo '<i class="fas ' . esc_attr( $icon ) . '" title="' . esc_attr__( "comment icon", "startup-blog" ) . '"></i>';
+		echo '<i class="' . esc_attr( $icon ) . '" title="' . esc_attr__( "comment icon", "startup-blog" ) . '"></i>';
 		echo '<span>';
 			comments_number( 0, 1, '%' );
 		echo '</span>';
